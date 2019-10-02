dfce90525df1c18e41c156d3c178e0eb38d6b9e2
Agustín <agustingonzalezmurua@gmail.com>
2019-10-02 15:17:25 -0300

    Altered setOwner policy to support "data" field when sending the entry with files

--- config/policies/setOwner.js
+++ config/policies/setOwner.js
@@ -2,7 +2,14 @@ module.exports = async (ctx, next) => {
   const { id } = ctx.state.user;
   const { body } = ctx.request;
 
-  body.owner = id.toString();
+  // If object is being sent with files
+  if (Object.prototype.hasOwnProperty.call(body, 'data') && typeof body.data === 'string') {
+    const _data = JSON.parse(body.data);
+    _data.owner = id.toString();
+    body.data = JSON.stringify(_data);
+  } else {
+    body.owner = id.toString();
+  }
 
   await next();
 };
